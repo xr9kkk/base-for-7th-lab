604115e3c144dc3f83201a59c8a3eeae332ce86d
Your Name <you@example.com>
2024-05-08 12:56:55 +0300

    как успеть

--- dynamic struct, linear list/Source.cpp
+++ dynamic struct, linear list/Source.cpp
@@ -36,6 +36,8 @@ bool empty(ptrNode head)
 //1) запросить новую память для элемента
 //2) заполнить поля нового узла
 //3) включить элемент в список
+
+//Функция добавления в голову
 void add_to_head(ptrNode& head, Tinfo elem)
 {
 	head = new NODE(elem, head);
@@ -103,9 +105,92 @@ void create_be_queue(ptrNode& head, std::ifstream& file)
 }
 
 /*Создание списка упорядоченно*/ 
+void create_by_order(ptrNode& head, std::ifstream& file)
+{
+	Tinfo elem;
+	init(head);
+	auto find_place = [&head](Tinfo elem)->ptrNode
+		{
+			ptrNode result = head;
+			while (result->next && result->next->info < elem)
+			{
+				result = result->next;
+			}
+			return result;
+		};
+	ptrNode ptr{};
+	while (file >> elem)
+	{
+		if (empty(head) || head->info >= elem)
+			add_to_head(head, elem);
+		else
+		{
+			ptr = find_place(elem);
+			add_after(ptr, elem);
+		}
+	}
+}
+
+void print(ptrNode head, const char* message)
+{
+	std::cout << message << '\n';
+	ptrNode ptr{ head };
+	while (ptr)
+	{
+		std::cout << ptr->info << ' ';
+		ptr = ptr->next;
+	}
+	std::cout << '\n';
+}
+
+void clear(ptrNode& head)
+{
+	while (empty(head))
+	{
+		del_from_head(head);
+	}
+}
+
+bool delete_all_even(ptrNode& head)
+{
+	bool result{ 0 };
+	while (!empty(head) && head->info % 2 == 0)
+	{
+		result = true;
+		del_from_head(head);
+	}
+	if (!empty(head))
+	{
+		ptrNode ptr{ head };
+		while (ptr->next)
+			if (ptr->next->info % 2 == 0)
+			{
+				result = true;
+				del_after(ptr);
+			}
+			else
+				ptr = ptr->next;
+
+	}
+	return result;
+}
 
 
-int main() {
 
-	system("pause");
+int main()
+{
+	
+	std::ifstream file("data.txt");
+	if (file) {
+		ptrNode head{};
+		create_by_stack(head, file);
+		print(head, "***");
+		clear(head);
+		delete_all_even(head);
+		print(head, "###");
+		clear(head);
+	}
+
+	std::cin.get();
+	return 0;
 }
